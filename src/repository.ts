96f9f6a99262911290e6b8fea4448f4a3a9aa425
Ian Homer <code@ianhomer.com>
2025-07-11 13:53:59 +0100

    docs: add some comments to guide purpose

--- src/repository.ts
+++ src/repository.ts
@@ -1,7 +1,11 @@
 import { Graph } from "@adaptivekind/graph-schema";
 import { RepositoryOptions } from "./types";
 
+// Create the repository
 export const createRepository = (options: RepositoryOptions) => {
+  // Create the graph of the markdown content where each markdown item in the repository
+  // is a single node, and any wiki-links in the markdown item is a link targetting the linked
+  // markdown item.
   const graph: Graph = {
     nodes: {},
     links: [],
